d1d2059a835a86433a1930f7c8b8fc652473ea8e
opafix <tcac2@cam.ac.uk>
2018-11-10 15:17:02 +0000

    added initial stuff to orientate.h (need more info and discussion on precise methods before we spec functions

---
+++ Software/source/orientation.h
@@ -0,0 +1,12 @@
+/*
+ * Contains functions to generate and manipulate 
+ * cartesian coordinate system and polygon objects
+ * used in pathing algorithms
+ */
+
+// ** Preamble ** //
+#include "firmware.h"
+
+// **** PUBLIC **** //
+
+// **** PRIVATE **** //
