fa99dc79351a5152c39f8ac790f83b73f99b87da
opafix <tcac2@cam.ac.uk>
2018-11-10 14:54:05 +0000

    added spec functions and descriptions to movement.h

---
+++ Software/source/movement.h
@@ -0,0 +1,106 @@
+/*
+ * Contains algorithms for moving the robot
+ */
+
+// ** Preamble ** //
+#include "firmware.h"
+
+// **** PUBLIC **** //
+
+// ** Facing ** //
+
+void faceFwd() {
+	// Points the robot forward relative to its course
+}
+
+void faceBwd() {
+	// Points the robot backward relative to its course
+}
+
+void faceLft() {
+	// Points the robot left relative to its course
+}
+
+void faceRgt() {
+	// Points he robot right relative to its course
+}
+
+void faceX+() {
+	// Points the robot in the positive X direction
+}
+
+void faceX-() {
+	// Points the robot in the negative X direction
+}
+
+void faceY+() {
+	// Points the robot in the positive Y direction
+}
+
+void faceY-() {
+	// Points the robot in the negative Y direction
+}
+
+void faceCoord+(coord input_coord) {
+	// Points the robot towards the coordinate "input_coord"
+}
+
+// ** Movement ** //
+
+void moveFwd(float input_distance) {
+	// Moves the robot forward (relative to current facing) by distance "input_distance"
+}
+
+void moveBwd(float input_distance) {
+	// Moved the robot backward (relative to current facing) byb distance "input_distance"
+}
+
+// ** Rotation ** //
+
+void spinLft(float input_degrees) {
+	// Rotates the robot left (anticlockwise) through angle "input_degrees"
+}
+
+void spinRgt(float input_degrees) {
+	// Rotates the robot right (clockwise) through angle "input_degrees"
+}
+
+// ** Path ** //
+
+void pathFollow+(float input_distance) {
+	// Moves the robot forwards along the path by distance "input_distance"
+}
+
+void pathEdge() {
+	// Moves the robot to the edge of the search area via the shortest route, avoiding mines
+}
+
+void pathReturn() {
+	// Moves the robot back to the point on its path where it left off, avoiding mines
+}
+
+void pathHome() {
+	// Moves the robot back to the startbox via the shortest route, avoiding mines
+}
+
+void pathGo(coord input_coord) {
+	// Moves the robot to the coordinate "input_coord" via the shortest route, avoiding mines
+}
+
+// ** Panic ** //
+
+void panicHome() {
+	// Moves the robot back to the start box, regardless of mines in the way
+}
+
+void panicEdge() {
+	// Moves the to the nearest edge, regardless of mines in the way
+}
+
+void panicRun() {
+	// Moves the robot in a straight line, regardless of mines, until it reaches the edge
+}
+
+void panicPanic() {
+	// Moves eratically and without direction in an inescapable loop (edgecase)
+}
