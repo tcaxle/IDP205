73346b558b722365c4aa9499d0a2c3574260f489
opafix <tcac2@cam.ac.uk>
2018-11-10 15:10:01 +0000

    added spec functions to indicate.h

---
+++ Software/source/indicate.h
@@ -0,0 +1,35 @@
+/*
+ * contains functions for controlling LEDs (or other output indicators)
+ */
+
+// ** Preamble ** //
+#include "firmware.h"
+
+// **** PUBLIC **** //
+
+// ** LED Control ** //
+
+void ledNone() {
+	// Turns on green LED only
+	// For indication of no mine detected
+}
+
+void ledDang() {
+	// Turns on red LED only
+	// For indication of dangerous mine detected
+}
+
+void ledSafe() {
+	// Turns on yellow LED only
+	// For indication of safe mine detected
+}
+
+void ledPanic() {
+	// Turns on all LEDs to indicate panic mode
+}
+
+void ledHappy() {
+	// Flashes LEDs to celebrate victory
+}
+
+// **** PRIVATE **** //
