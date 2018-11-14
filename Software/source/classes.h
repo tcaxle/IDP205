ced1ce12cd349b32e2235603b30f69c992b29046
opafix <tcac2@cam.ac.uk>
2018-11-14 10:58:32 +0000

    some new classes (middle, upper, lower, upper-middle, lower-middle, I could go on) and a new capitalist society based on the echange currency of chicken eggs

--- Software/source/classes.h
+++ Software/source/classes.h
@@ -91,7 +91,68 @@ class motor {
 };
 
 class coord {
-  public:
-    int x, y;
+	public:
+		int x, y;
+		coord add(coord inputCoord) {
+			// Returns a coordinate that is the original plus the input elementwise
+			coord outputCoord;
+			outputCoord.x = x + inputCoord.x;
+			outputCoord.y = y + inputCoord.y;
+			return outputCoord;
+		}
+		coord subtract(coord inputCoord) {
+			// Returns a coordinate that is the original minus the input elementwise
+			coord outputCoord;
+			outputCoord.x = x - inputCoord.x;
+			outputCoord.y = y - inputCoord.y;
+			return outputCoord;
+		}
+		float distance(coord inputCoord) {
+			// Returns the distance between two coordinates
+			coord difference = subtract(inputCoord);
+			return pow(pow(difference.x,2) + pow(difference.y,2), 0.5);
+    		}
 };
 
+class rectangle {
+	/* A class used to denote "keepout zones"
+	 *     x0    x1
+	 *      |    |
+	 *  y1--a----b--
+	 *      |    |
+	 *  y0--c----d--
+	 *      |    |
+	 */
+	public:
+		int x0, x1, y0, y1;
+		coord a, b, c, d;
+		void init() {
+			// Creates the coordinates for the corners, a, b, c, d (see diagram above)
+			a.x = x0; a.y = y1;
+			b.x = x1; b.y = y1;
+			c.x = x0; c.y = y0;
+			d.x = x1; d.y = y0;
+		}
+		bool insideRectangle(coord inputCoord) {
+			// Returns True if "inputCoord" is inside the rectangle object. Else returns False.
+			if ( (inputCoord.x > x0 && inputCoord.x < x1) || (inputCoord.y > y0 && inputCoord.y < y1) ) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+		coord nearestCorner(coord inputCoord) {
+			// Returns the coordinate of the nearest corner to the "inputCoord"
+			coord corner = a;
+			if (inputCoord.distance(b) < inputCoord.distance(corner)) {
+				corner = b;
+			}
+			if (inputCoord.distance(c) < inputCoord.distance(corner)) {
+				corner = c;
+			}
+			if (inputCoord.distance(d) < inputCoord.distance(corner)) {
+				corner = d;
+			}
+			return corner;
+		}
+};
