fc4dcd231f94087e11164137f8f18baf508985ba
Grelkis <lewis.a.j.anderson98@gmail.com>
2018-11-16 15:55:06 +0000

    Complete provisional getDirection function (untested)

--- Software/source/orientation.h
+++ Software/source/orientation.h
@@ -13,4 +13,22 @@
 
 // **** PUBLIC **** //
 
+// ** Status **//
+void getCoords() {
+    // Uses the ultrasound sensors to return a coord object giving the robot’s current location
+}
+
+int getDirection(compass targetCompass, compassOffset = 0) {
+    //Uses the compass and the orientated compass offset to return the robot’s angle
+	//Get the initial compass reading
+	float compassReading = targetCompass.getHeading();
+	//Correct reading to be relative to x-axis heading then output
+	if (compassOfsset - compassReading <= -180) {
+		return round(360 + compassOffset - compassReading)
+	}
+	else{
+		return round (compassOffset - compassReading)
+	}
+}
+
 // **** PRIVATE **** //
