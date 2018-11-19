9b076765c4e260272b56b136720f0a33bb574374
Grelkis <lewis.a.j.anderson98@gmail.com>
2018-11-19 11:22:03 +0000

    Updated test_compass_1 to test getDirection

--- Software/tests/test_compass_1/test_compass_1.ino
+++ Software/tests/test_compass_1/test_compass_1.ino
@@ -10,10 +10,14 @@
 #include "libraries.h"
 #include "classes.h"
 #include "firmware.h"
+#include "orientation.h"
 
 
 void loop() {
   float currentHeading = robotCompass.heading();
+  int currentDirection = getDirection(robotCompass);
   Serial.print("Compass Heading: ");
   Serial.println(currentHeading);
+  Serial.print("Cartesian Direction: ");
+  Serial.println(currentDirection);
 }
