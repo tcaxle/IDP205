1b893675b36e3ee71fc8cfa5f9d3aabbcbb5923d
Grelkis <lewis.a.j.anderson98@gmail.com>
2018-11-20 14:38:01 +0000

    Filled out getCoords

--- Software/source/orientation.h
+++ Software/source/orientation.h
@@ -14,20 +14,42 @@
 // **** PUBLIC **** //
 
 // ** Status **//
-void getCoords() {
+coord getCoords(float areaX, float areaY, float robotLength, float robotWidth, float currentDirection) {
     // Uses the ultrasound sensors to return a coord object giving the robot’s current location
-}
-
-int getDirection(compass targetCompass, compassOffset = 0) {
-    //Uses the compass and the orientated compass offset to return the robot’s angle
-	//Get the initial compass reading
-	float compassReading = targetCompass.getHeading();
-	//Correct reading to be relative to x-axis heading then output
-	if (compassOfsset - compassReading <= -180) {
-		return round(360 + compassOffset - compassReading)
+	// Lengths to be given in cm
+	// Direction to be given in degrees and should be oriented to the robot's Cartesian co-ordinate system
+	if (abs(currentDirection) <= 45){
+		faceX+();
+		xCoordinate = xUltrasound.getReading() + robotLength;
+		yCoordinate = areaY - (yUltrasound.getReading() + robotWidth/2);
+	}
+	else if (currentDirection > 0) && (currentDirection <= 135){
+		faceY+();
+		xCoordinate = yUltrasound.getReading() + robotWidth/2;
+		yCoordinate = xUltrasound.getReading() + robotLength;
+	}
+	else if (currentDirection < 0) && (currentDirection >= -135){
+		faceY-();
+		xCoordinate = areaX - (yUltrasound.getReading() + robotWidth/2);
+		yCoordinate = areaY - (xUltrasound.getReading() + robotLength);
 	}
 	else{
-		return round (compassOffset - compassReading)
+		faceX-();
+		xCoordinate = areaX - (xUltrasound.getReading + robotLength);
+		yCoordinate = yUltrasound.getReading() + robotWidth/2;
+	}
+	coord currentCooord(xCoordinate, yCoordinate);
+	return currentCoord;
+}
+
+int getDirection(float currentHeading, compassOffset = 0) {
+    //Uses a reading from the compass and the orientated compass offset to return the robot’s angle
+	//Correct reading to be relative to x-axis heading then output
+	if (compassOfsset - compassReading <= -180) {
+		return round(360 + compassOffset - currentHeading);
+	}
+	else{
+		return round (compassOffset - currentHeading);
 	}
 }
 
