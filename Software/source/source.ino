3ebd73aef941dad129f60932a8fa9cff7bbeca7b
Grelkis <lewis.a.j.anderson98@gmail.com>
2018-11-23 14:35:19 +0000

    initialiseOrientation test code transferred from source to test_initialiseOrientation

--- Software/source/source.ino
+++ Software/source/source.ino
@@ -16,43 +16,10 @@
 #include "orientation.h" // 5
 #include "movement.h" // 6
 
-// Initialise circular path co-ordinates for pathFollow test
-coord point1(50, 50);
-coord point2(50, 100);
-coord point3(50, 150);
-coord point4(100, 150);
-coord point5(150, 150);
-coord point6(150, 100);
-coord point7(150, 50);
-coord point8(100, 50);
-
 // Initialise sigle destination co-ordinate for pathGo test
 coord target(50, 50);
 
 void loop() {
-    /*//pathFollow test
-    // On first run
-    if (runCounter == 0){
-        compassOffset = initialiseOrientation();
-        Serial.println("Ready!");
-        delay(8000);
-    }
-    // Populate path
-    Serial.print("Populating... ");
-    path.push_back(point8);
-    path.push_back(point7);
-    path.push_back(point6);
-    path.push_back(point5);
-    path.push_back(point4);
-    path.push_back(point3);
-    path.push_back(point2);
-    path.push_back(point1);
-    Serial.println("Done!");
-    Serial.println();
-    // Follow path
-    pathFollow(path);
-    runCounter += 1;*/
-    
 
     /*//initialiseOrientation test
     if (runCounter == 0){
